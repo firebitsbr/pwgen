ee065f68d49798bb69c7d30204e155f2da3d41cb
David J Peacock <david@davidjpeacock.ca>
2016-09-17 18:36:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# pwgen
+
+[![Build Status](https://travis-ci.org/davidjpeacock/pwgen.svg?branch=master)](https://travis-ci.org/davidjpeacock/pwgen)
+
+Cryptographically secure password generator implemented in Golang.
+
+Supports arbitrary password length, and alpha, numeric, and alphanumeric character sets.
+
+## Installation
+
+```
+go get github.com/davidjpeacock/pwgen
+```
+
+## Usage
+
+```
+Usage of pwgen:
+  -charset string
+        Character set: alpha, numeric, alphanumeric (default "alphanumeric")
+  -length int
+        Password length (default 16):
+```
+
+## License
+
+ MIT
+  
+## Contributing
+
+  Pull requests welcome.
---
+++ main.go
@@ -0,0 +1,47 @@
+// Cryptographically secure password generator.
+//
+// Copyright (c) 2016 David Peacock - david.j.peacock@gmail.com
+
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"fmt"
+)
+
+var length = flag.Int("length", 16, "Password length")
+var charset = flag.String("charset", "alphanumeric", "Character set: alpha, numeric, alphanumeric")
+
+func main() {
+	flag.Parse()
+
+	charSetOption := characterSets(*charset)
+
+	fmt.Println(generator(*length, charSetOption))
+}
+
+func characterSets(charSetOption string) string {
+	var chars string
+
+	if charSetOption == "alpha" {
+		chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	}
+	if charSetOption == "numeric" {
+		chars = "0123456789"
+	}
+	if charSetOption == "alphanumeric" {
+		chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	}
+	return string(chars)
+}
+
+func generator(length int, charSet string) string {
+	password := make([]byte, length)
+	rand.Read(password)
+
+	for k, v := range password {
+		password[k] = charSet[v%byte(len(charSet))]
+	}
+	return string(password)
+}
